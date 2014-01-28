52550623f8675b8bdfee8cd9c534582632f1a703
Tiago Furlanetto <tiago.f.furlanetto@gmail.com>
2014-01-28 11:04:57 -0200

    SingleSectioned Web DS Refactor.

--- PodContent/UZKSingleSectionedWebDS.h
+++ PodContent/UZKSingleSectionedWebDS.h
@@ -6,8 +6,40 @@
 //  Copyright (c) 2014 Uzaak. All rights reserved.
 //
 
-#import "UZKWebDS.h"
+#import <Foundation/Foundation.h>
 
-@interface UZKSingleSectionedWebDS : UZKWebDS
+#import "UZKWebDSClient.h"
+
+@interface UZKSingleSectionedWebDS : NSObject <UICollectionViewDataSource, UITableViewDataSource>
+
+@property (nonatomic, weak) IBOutlet UICollectionView * collectionView;
+@property (nonatomic, weak) IBOutlet UITableView * tableView;
+
+@property (nonatomic, strong) NSString * cellIdentifier;
+@property (nonatomic, strong) NSString * (^cellIdentifierBlock)(id);
+@property (nonatomic, strong) void (^cellDequeueBlock)(id);
+
+@property (nonatomic, strong) NSString * (^reusableViewIdentifierBlock)(NSString * kind, NSIndexPath * indexPath);
+@property (nonatomic, strong) void (^reusableViewDequeueBlock)(UICollectionReusableView * reusableView);
+
+@property (nonatomic, strong) void (^requestCallbackStartBlock)(NSArray * retrievedContent);
+@property (nonatomic, strong) void (^requestCallbackFinishBlock)(NSArray * retrievedContent);
+
+@property (nonatomic, strong) IBOutlet id<UZKWebDSClient> client;
+
+@property (nonatomic, strong) NSDictionary * parameters;
+
+@property (nonatomic) NSInteger sectionIndexOffset;
+
+- (void)setSectionData:(NSArray *)dataForSection0, ... NS_REQUIRES_NIL_TERMINATION;
+
+- (id)objectForIndexPath:(NSIndexPath *)indexPath;
+
+- (BOOL)hasNoResults;
+
+- (void)resetData;
+
+// Forces load of next page. If you don't know what it's for, you probably don't need to use it.
+- (void)forceLoadNextPage;
 
 @end
--- PodContent/UZKSingleSectionedWebDS.m
+++ PodContent/UZKSingleSectionedWebDS.m
@@ -25,6 +25,56 @@
 }
 
 
+- (id)init
+{
+    self = [super init];
+    
+    if ( self )
+    {
+        self.cellIdentifier = @"Cell";
+        self.pages = [NSMutableArray new];
+    }
+    
+    return self;
+}
+
+#pragma mark Gambizarra
+
+- (void)setCollectionView:(UICollectionView *)collectionView
+{
+    _collectionView = collectionView;
+    
+    [_collectionView registerNib:[UINib nibWithNibName:@"UZKWebDSLoadMoreCollectionCell" bundle:nil] forCellWithReuseIdentifier:@"UZKWebDSLoadMoreCollectionCell"];
+    
+    [_collectionView registerNib:[UINib nibWithNibName:@"UZKWebDSNoResultsCollectionCell" bundle:nil] forCellWithReuseIdentifier:@"UZKWebDSNoResultsCollectionCell"];
+}
+
+- (void)setTableView:(UITableView *)tableView
+{
+    _tableView = tableView;
+    
+    [_tableView registerNib:[UINib nibWithNibName:@"UZKWebDSLoadMoreTableCell" bundle:nil] forCellReuseIdentifier:@"UZKWebDSLoadMoreTableCell"];
+    
+    [_tableView registerNib:[UINib nibWithNibName:@"UZKWebDSNoResultsTableCell" bundle:nil] forCellReuseIdentifier:@"UZKWebDSNoResultsTableCell"];
+}
+
+
+#pragma mark Initial Stuff
+
+- (void)setSectionData:(NSArray *)dataForSection0, ... NS_REQUIRES_NIL_TERMINATION
+{
+    [self resetData];
+    
+    va_list args;
+    va_start(args, dataForSection0);
+    for (NSArray * dataForSection = dataForSection0; dataForSection != nil; dataForSection = va_arg(args, NSArray *))
+    {
+        [self.pages addObject:dataForSection];
+    }
+    va_end(args);
+}
+
+
 #pragma mark COLLECTION
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
@@ -86,6 +136,26 @@
     }
     
     return cell;
+}
+
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    if ( !self.reusableViewIdentifierBlock )
+    {
+        return nil;
+    }
+    
+    NSString * reusableViewIdentifier = self.reusableViewIdentifierBlock(kind, indexPath);
+    
+    UICollectionReusableView * reusableView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:reusableViewIdentifier forIndexPath:indexPath];
+    
+    if ( self.reusableViewDequeueBlock )
+    {
+        self.reusableViewDequeueBlock(reusableView);
+    }
+    
+    return reusableView;
 }
 
 
@@ -171,5 +241,112 @@
     
     return nil;
 }
+
+
+#pragma mark Async loading
+
+- (void)loadPage:(NSNumber *)n
+{
+    if (loading) return;
+    loading = YES;
+    
+    int number = [n intValue];
+    
+    void(^successBlock)(NSArray * stuff) = ^(NSArray * stuff)
+    {
+        [refreshControl endRefreshing];
+        
+        if ( self.requestCallbackStartBlock )
+        {
+            self.requestCallbackStartBlock(stuff);
+        }
+        
+        if (![stuff count] && number == 0) {
+            // Primeira página sem resultados
+            finished = YES;
+            [self performSelectorOnMainThread:@selector(animateNothingReallyPage)
+                                   withObject:nil
+                                waitUntilDone:NO];
+        } else if (![stuff count]) {
+            // Última página
+            finished = YES;
+            [self performSelectorOnMainThread:@selector(animateLastPage)
+                                   withObject:nil
+                                waitUntilDone:NO];
+        } else {
+            [self.pages addObject:stuff];
+            [self performSelectorOnMainThread:@selector(animatePageInsertion:)
+                                   withObject:n
+                                waitUntilDone:NO];
+        }
+        
+        loading = NO;
+        
+        if ( self.requestCallbackFinishBlock )
+        {
+            self.requestCallbackFinishBlock(stuff);
+        }
+    };
+    
+    [self.client requestPage:number + 1
+              withParameters:self.parameters
+                successBlock:successBlock];
+}
+
+#pragma mark Animating Insertions
+
+- (void)animatePageInsertion:(NSNumber *)page
+{
+    [self updateCollectionAnimator];
+    [self.collectionView reloadData];
+    [self.tableView reloadData];
+}
+
+- (void)animateLastPage
+{
+    [self updateCollectionAnimator];
+    [self.collectionView reloadData];
+    [self.tableView reloadData];
+}
+
+- (void)animateNothingReallyPage
+{
+    [self updateCollectionAnimator];
+    [self.collectionView reloadData];
+    [self.tableView reloadData];
+}
+
+- (void)updateCollectionAnimator
+{
+    if ( [self.collectionView.collectionViewLayout respondsToSelector:@selector(updateAnimator)] )
+    {
+        [self.collectionView.collectionViewLayout performSelector:@selector(updateAnimator)];
+    }
+}
+
+
+#pragma mark You can't change what isn't there
+
+- (BOOL)hasNoResults
+{
+    return ( [self.pages count] == 0 );
+}
+
+
+#pragma mark Start All Over
+
+- (void)resetData
+{
+    self.pages = [NSMutableArray new];
+    finished = NO;
+    loading = NO;
+}
+
+
+- (void)forceLoadNextPage
+{
+    [self performSelectorOnMainThread:@selector(loadPage:) withObject:@([self.pages count]) waitUntilDone:NO];
+}
+
 
 @end
